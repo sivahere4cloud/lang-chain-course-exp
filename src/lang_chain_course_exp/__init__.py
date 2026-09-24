adbd0b264af66e1c2abfa51efedce058fc31515e
sivahere4cloud <sivaganeshcloud@gmail.com>
2026-09-24 22:35:28 +0100

    swiching llm from openai to ollama

--- src/lang_chain_course_exp/__init__.py
+++ src/lang_chain_course_exp/__init__.py
@@ -1,6 +1,8 @@
+
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import  PromptTemplate
+from langchain_ollama import ChatOllama
 
 
 load_dotenv()
@@ -22,7 +24,8 @@ def main() -> None:
     summary_prompt_template = PromptTemplate(
         input_variables={"information"},template=summary_template
     )
-    llm = ChatOpenAI(temperature = 0,model = "gpt-5")
+    ##llm = ChatOpenAI(temperature = 0,model = "gpt-5")
+    llm = ChatOllama(temperature=0, model="gemma3:1b")
     chain = summary_prompt_template | llm
 
 
