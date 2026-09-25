31247c0dfae8e32a915a70288629ea0a9db569c8
sivahere4cloud <sivaganeshcloud@gmail.com>
2026-09-25 10:08:05 +0100

    Weather search  using python tavilyclient

--- src/lang_chain_course_exp/__init__.py
+++ src/lang_chain_course_exp/__init__.py
@@ -1,17 +1,49 @@
 from dotenv import load_dotenv
-import os
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+
 load_dotenv()
 
 
-def main() -> None:
-    print("Hello from search-agent!")
-    tavily_key = os.environ.get("TAVILY_API_KEY")
-    openai_key = os.environ.get("OPENAI_API_KEY")
-    langsmith_project = os.environ.get("LANGSMITH_PROJECT")
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over the internet.
 
-    print("Tavily key found:", tavily_key is not None)
-    print("OpenAI key found:", openai_key is not None)
-    print("LangSmith project:", langsmith_project)
+    Args:
+        query: The query to search for.
+
+    Returns:
+        The search result.
+    """
+    print(f"Searching for {query}")
+    return "Tokyo weather is sunny"
+
+
+def main() -> None:
+    llm = ChatOpenAI(model="gpt-5-mini")
+    tools = [search]
+    agent = create_agent(model=llm, tools=tools)
+
+    result = agent.invoke(
+        {"messages": [HumanMessage(content="What is the weather in Tokyo?")]}
+    )
+
+    print("Type of result:", type(result))
+    print("Keys:", result.keys())
+
+    for message in result["messages"]:
+        print("-----")
+        print("Message type:", type(message).__name__)
+        print("Content:", message.content)
+
+        if type(message).__name__ == "AIMessage":
+            print("Tool calls:", message.tool_calls)
+
+        if type(message).__name__ == "ToolMessage":
+            print("Answers tool call id:", message.tool_call_id)
 
 if __name__ == "__main__":
     main()
